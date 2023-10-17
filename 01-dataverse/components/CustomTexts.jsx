e2fe2d37a0e5b070ff9cc09ab9409b1f567ace22
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-17 14:31:02 +0000

    feat: feat/01-dataverse/about - add intro typing text for about page

--- 01-dataverse/components/CustomTexts.jsx
+++ 01-dataverse/components/CustomTexts.jsx
@@ -1,7 +1,24 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.div
+    variants={textContainer}
+    className="w-full relative text-center"
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span
+        variants={textVariant2}
+        key={index}
+        className={`${textStyles} opacity-20 text-white font-normal sm:text-[25px] text-[18px]`}
+      >
+        {letter}
+      </motion.span>
+    ))}
+  </motion.div>
+
 );
 
 export const TitleText = () => (
--- 01-dataverse/sections/About.jsx
+++ 01-dataverse/sections/About.jsx
@@ -1,8 +1,20 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { staggerContainer } from '../utils/motion';
+import styles from '../styles';
+import { TypingText } from '../components/CustomTexts';
+
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.xPaddings} w-full relative`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+    >
+      <TypingText title="| About Dataversus" textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
