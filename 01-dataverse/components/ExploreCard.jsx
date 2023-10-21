c25643ef504c5429bea71c9f5c78a19c29c6f062
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-21 22:21:02 +0000

    add: stable version of the entire website.

--- 01-dataverse/components/ExploreCard.jsx
+++ 01-dataverse/components/ExploreCard.jsx
@@ -17,15 +17,15 @@ const ExploreCard = ({ index, world, active, handleClick }) => (
     { active !== world.id ? (
       <h3 className="font-normal text-white sm:text-[26px] text-[18px] z-0 lg:absolute lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0, 0]">{world.title}</h3>
     ) : (
-      <div className="absolute bottom-0 left-0 right-0 bg-[rgba(0,0,0,0.5)] p-6 z-0 rounded-b-[24px]">
-        <div className="flex items-center justify-center md:w-[60px] w-[40px] md:h-[60px] h-[40px] glassmorphism mb-[20px] md:rounded-[24px] rounded-[15px]">
+      <div className="absolute bottom-0 w-full bg-[rgba(0,0,0,0.5)] z-0 rounded-b-[24px] md:p-6 p-3">
+        <div className="flex items-center justify-center md:w-[60px] w-[40px] md:h-[60px] h-[40px] glassmorphism md:mb-[20px] mb-[10px] md:rounded-[24px] rounded-[15px]">
           <img
             src="/headset.svg"
             className="w-1/2 h-1/2 object-contain"
           />
         </div>
-        <p className="font-normal uppercase text-[16px] text-white">Enter the Dataverse</p>
-        <h2 className="font-bold text-[24px] md:text-[36px] text-white">{world.title}</h2>
+        <p className="font-normal uppercase sm:text-[16px] text-[12px] text-white">Enter the Dataverse</p>
+        <h2 className="font-bold sm:text-[24px] text-[18px] md:text-[36px] text-white">{world.title}</h2>
       </div>
     )}
   </motion.div>
