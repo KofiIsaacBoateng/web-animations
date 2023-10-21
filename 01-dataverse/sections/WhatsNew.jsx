39d7ee081f1dc0fbe0f47345dd68bd7772b5191d
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-21 23:38:59 +0000

    feat: started what's-new section

--- 01-dataverse/sections/WhatsNew.jsx
+++ 01-dataverse/sections/WhatsNew.jsx
@@ -1,8 +1,34 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import styles from '../styles';
+import { TitleText, TypingText } from '../components';
+
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className={`${styles.yPaddings} flex flex-col relative`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} flex mx-auto md:flex-row flex-col`}
+    >
+      <div className="md:flex-[0.75] flex-1">
+        <TypingText title="| What's New ?" textStyles="text-left" />
+        <TitleText title="What's new about Dataversus ?" textStyles="text-center" />
+      </div>
+      <motion.div
+        variants={planetVariants('right')}
+        className="flex-1 w-full md:w-[30%]"
+      >
+        <img
+          src="/whats-new.png"
+          alt="what's new"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
   </section>
 );
 
