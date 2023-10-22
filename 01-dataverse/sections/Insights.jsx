b44e792cd3ad52916738d864e4cdb0b8864c3727
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-22 10:14:13 +0000

    feat: insights started

--- 01-dataverse/sections/Insights.jsx
+++ 01-dataverse/sections/Insights.jsx
@@ -1,8 +1,22 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles'
+import { TitleText, TypingText } from '../components';
+import { staggerContainer } from '../utils/motion';
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.yPaddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto relative`}
+    >
+      <TypingText title="| Insights" textStyles="text-center" />
+      <TitleText title="Know more about the metaverse" textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
