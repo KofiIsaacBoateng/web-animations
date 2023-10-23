2e9c9d39a38411c71f1dcf107e30984e9beb930e
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-23 10:50:29 +0000

    fix: animations now synching well for insights card

--- 01-dataverse/components/InsightCard.jsx
+++ 01-dataverse/components/InsightCard.jsx
@@ -12,7 +12,7 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     className="px-8 md:px-0 flex md:flex-row flex-col md:justify-center items-center gap-3 md:gap-8"
   >
     <motion.div
-      variants={fadeIn('left', 'spring', index * 0.25, 1)}
+      variants={fadeIn('right', 'spring', index * 0.25, 1)}
       className="relative md:w-[25%] w-full min-h-[250px] z-[10] rounded-[24px] overflow-hidden mb-4 md:mb-0"
     >
       <img
@@ -22,12 +22,23 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
       />
     </motion.div>
     <motion.div
-      variants={fadeIn('right', 'spring', index * 0.25, 1)}
-      className="flex flex-col gap-2 md:w-[60%] self-start"
+      variants={fadeIn('up', 'spring', index * 0.25, 1)}
+      className="flex flex-col gap-2 md:w-[45%] self-start"
     >
-      <h3 className="font-bold text-white text-[26px] md:text-[35px]">{title}</h3>
+      <h3 className="font-bold text-white text-[26px] md:text-[32px]">{title}</h3>
       <p className="text-secondary-white text-[18px] md:text-[25px]">{subtitle}</p>
     </motion.div>
+
+    <motion.div
+      variants={fadeIn('left', 'spring', index * 0.25, 1)}
+      className="hidden md:flex items-center justify-center border rounded-full self-start p-8"
+    >
+      <img
+        src="/arrow.svg"
+        alt="arrow pointing top right"
+      />
+    </motion.div>
+
   </motion.div>
 );
 
