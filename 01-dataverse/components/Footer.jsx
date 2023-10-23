18c0db845ead5ec147709dee8fe038c7d37f16dd
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-23 13:34:12 +0000

    feat: footer completed

--- 01-dataverse/components/Footer.jsx
+++ 01-dataverse/components/Footer.jsx
@@ -10,16 +10,42 @@ const Footer = () => (
         <TitleText title="Enter the Dataverse" />
         <button
           type="button"
-          className="flex gap-5 items-center justify-between md:py-[15px] py-5 md:px-[30px] px-8 bg-[firebrick] self-start md:rounded-[24px] rounded-[15px]"
+          className="flex gap-5 items-center justify-between md:py-[15px] py-4 md:px-[30px] px-5 bg-[#25618b] md:self-center self-start rounded-[40px]"
         >
           <img
             src="/headset.svg"
-            className="w-[30px] h-[30px] "
+            className="w-[20px] md:w-[30px] h-[20px] md:h-[30px] "
           />
           <p className="text-[18px] md:text-[26px] text-white ">Enter Dataverse</p>
         </button>
       </div>
       <hr />
+      <div className="flex md:flex-row flex-col md:items-center md:justify-between gap-10 mt-[50px] ">
+        <div className="footer-gradient" />
+        <h4 className="text-white text-[22px] font-semibold">Dataversus</h4>
+        <p className="text-secondary-white sm:text-[18px] text-[15px] md:mx-auto">Copyright @2023 Dataversus. All rights reserved</p>
+        <div className="flex items-center justify-space-between gap-6 ">
+          <img
+            src="/facebook.svg"
+            alt="facebook"
+            className="w-[20px] h-[20px] "
+          />
+          <img
+            src="/twitter.svg"
+            alt="facebook"
+            className="w-[20px] h-[20px]"
+          />
+          <img
+            src="/instagram.svg"
+            alt="facebook"
+          />
+          <img
+            src="/linkedin.svg"
+            alt="facebook"
+            className="w-[20px] h-[20px]"
+          />
+        </div>
+      </div>
     </div>
   </footer>
 );
